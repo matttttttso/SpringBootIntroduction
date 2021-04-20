dcbf480583ebae119443240ef6e9290ba0562e66
kis_ymatsuo <ymatsuo@kis>
2021-04-20 14:02:22 +0900

    13.3.1 Spring JDBC (PostgreSQL) 動作確認無し

--- Introduction/src/main/resources/schema.sql
+++ Introduction/src/main/resources/schema.sql
@@ -1,6 +1,4 @@
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS messages (
 	id		INT PRIMARY KEY AUTO_INCREMENT,
 	text	VARCHAR(255)
 );
-INSERT INTO messages(text) VALUES ('test_text_1');
-INSERT INTO messages(text) VALUES ('test_text_2');

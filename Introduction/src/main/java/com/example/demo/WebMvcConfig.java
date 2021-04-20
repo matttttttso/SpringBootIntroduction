06965836069ec22c8dc87f26901b3ef6174c22c2
kis_ymatsuo <ymatsuo@kis>
2021-04-20 11:40:16 +0900

    13.2.2-3 画面遷移型アプリケーションの作成-3 WebMvcConfigurerインターフェイスの使用例

---
+++ Introduction/src/main/java/com/example/demo/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer{
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+	}
+}

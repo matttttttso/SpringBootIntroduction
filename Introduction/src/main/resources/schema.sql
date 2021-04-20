3a1faa983218d8165893e08ded36cd71d907a393
kis_ymatsuo <ymatsuo@kis>
2021-04-20 12:28:51 +0900

    13.3.1 Spring JDBC (H2) 動作確認

--- Introduction/src/main/resources/schema.sql
+++ Introduction/src/main/resources/schema.sql
@@ -2,3 +2,5 @@ CREATE TABLE messages (
 	id		INT PRIMARY KEY AUTO_INCREMENT,
 	text	VARCHAR(255)
 );
+INSERT INTO messages(text) VALUES ('test_text_1');
+INSERT INTO messages(text) VALUES ('test_text_2');

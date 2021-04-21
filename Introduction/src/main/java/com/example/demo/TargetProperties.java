16f61fc10b1926d214e9008d4257b7d084b5b6b2
kis_ymatsuo <ymatsuo@kis>
2021-04-21 12:18:05 +0900

    13.5.2 Bean Validationによるプロパティ値のチェック

--- Introduction/src/main/java/com/example/demo/TargetProperties.java
+++ Introduction/src/main/java/com/example/demo/TargetProperties.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "target")
 public class TargetProperties {
+	@NotEmpty
 	private String host;
+	@Min(0)
+	@Max(65535)
 	private int port;
 	
 	public String getHost() {return host;}

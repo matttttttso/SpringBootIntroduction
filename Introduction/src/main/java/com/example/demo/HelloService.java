9eba7a31696cc0a15908fb864102b86a533a9b37
kis_ymatsuo <ymatsuo@kis>
2021-04-21 12:03:03 +0900

    13.5 Spring Bootで型安全なプロパティ設定

---
+++ Introduction/src/main/java/com/example/demo/HelloService.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloService {
+	@Autowired
+	TargetProperties targetProperties;
+	
+	public String hello() {
+		String target = "http://" + targetProperties.getHost() + ":" + targetProperties.getPort();
+		return target;
+	}
+}

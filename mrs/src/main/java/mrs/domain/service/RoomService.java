700e1557d4cd07acf2800b58406f552705a23f92
kis_ymatsuo <ymatsuo@kis>
2021-04-22 14:03:08 +0900

    14.2.7 会議室一覧表示機能の実装-1

---
+++ mrs/src/main/java/mrs/domain/service/RoomService.java
@@ -0,0 +1,23 @@
+package mrs.domain.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import mrs.domain.model.ReservableRoom;
+import mrs.domain.repository.reservation.ReservableRoomRepository;
+
+@Service
+@Transactional
+public class RoomService {
+	
+	@Autowired
+	ReservableRoomRepository reservableRoomRepository;
+	
+	public List<ReservableRoom> findReservableRooms(LocalDate date) {
+		return reservableRoomRepository.findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(date);
+	}
+}

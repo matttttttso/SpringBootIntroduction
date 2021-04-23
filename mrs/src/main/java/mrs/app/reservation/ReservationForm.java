4ce6f090ef3514b574d2eda76b5b07b68d393c79
kis_ymatsuo <ymatsuo@kis>
2021-04-23 16:32:41 +0900

    14.2.8 会議室予約機能の実装 -3 コントローラクラスの作成-1

---
+++ mrs/src/main/java/mrs/app/reservation/ReservationForm.java
@@ -0,0 +1,24 @@
+package mrs.app.reservation;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("serial")
+@Getter
+@Setter
+public class ReservationForm implements Serializable{
+	@NotNull(message = "必須です")
+	@DateTimeFormat(pattern = "HH:mm")
+	private LocalTime startTime;
+	
+	@NotNull(message = "必須です")
+	@DateTimeFormat(pattern = "HH:mm")
+	private LocalTime endTime;
+}

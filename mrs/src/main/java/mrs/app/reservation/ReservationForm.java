a3c8b29882fd744aac218a71d438528341ce2fed
kis_ymatsuo <ymatsuo@kis>
2021-04-26 11:03:02 +0900

    14.2.8 会議室予約機能の実装 -4 入力チェックの実装

--- mrs/src/main/java/mrs/app/reservation/ReservationForm.java
+++ mrs/src/main/java/mrs/app/reservation/ReservationForm.java
@@ -13,12 +13,15 @@ import lombok.Setter;
 @SuppressWarnings("serial")
 @Getter
 @Setter
+@EndTimeMustBeAfterStartTime(message = "終了時刻は開始時刻より後にしてください")
 public class ReservationForm implements Serializable{
 	@NotNull(message = "必須です")
+	@ThirtyMinutesUnit(message = "30分単位で入力して下さい")
 	@DateTimeFormat(pattern = "HH:mm")
 	private LocalTime startTime;
 	
 	@NotNull(message = "必須です")
+	@ThirtyMinutesUnit(message = "30分単位で入力して下さい")
 	@DateTimeFormat(pattern = "HH:mm")
 	private LocalTime endTime;
 }

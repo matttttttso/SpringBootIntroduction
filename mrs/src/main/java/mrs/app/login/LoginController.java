c37050447ddcf3de8d8e409264debab71df3c964
matttttttso <u2.pinetail@gmail.com>
2021-08-23 17:13:23 +0900

    14.2.9 ログイン機能の実装 ログイン画面の作成

---
+++ mrs/src/main/java/mrs/app/login/LoginController.java
@@ -0,0 +1,11 @@
+package mrs.app.login;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+//	@RequestMapping("loginForm")
+//	String loginForm() {
+//		return "login/loginForm";
+//	}
+}
